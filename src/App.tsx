bc38fafd0935fb583a855c6811fb1e2d48bc6d94
Daniel <740907068@qq.com>
2021-01-14 16:23:26 +0800

    导航栏布局搞定

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,92 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+   flex-direction: column;
+`;
+const Main = styled.div`
+  border: 1px solid green;
+  flex-grow: 1;
+  overflow: auto;
+`;
+const Nav = styled.nav`
+  border: 1px solid blue;
+  >ul{
+    width: 100%;
+    display: flex;
+    >li{
+      width: 33.33%;
+      text-align: center;
+      padding: 16px;
+      color: #000;
+    }
+  }
+display: flex;
+`;
 
 function App() {
   return (
-    <div>Hello</div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Route path="*">
+              <NotFount></NotFount>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NotFount() {
+  return <h2>该页面不存在</h2>;
 }
 
 export default App;
