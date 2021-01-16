5e2992faa2be1478e0c4a174c8715fac39e20e50
Daniel <740907068@qq.com>
2021-01-16 10:09:26 +0800

    数字面板按钮功能基本完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import {TagsSection} from './Money/TagsSection';
 import {Category} from './Money/CategorySection';
 import {NoteSection} from './Money/NoteSection';
-import {NumberPadSection} from './Money/NumberPadSection';
+import {NumberPad} from './Money/NumberPadSection';
 
 const MyLayout = styled(Layout)`
   display:flex;
@@ -17,27 +17,7 @@ function Money() {
       </TagsSection>
       <NoteSection></NoteSection>
       <Category></Category>
-      <NumberPadSection>
-        <div className="output">
-          100
-        </div>
-        <div className="pad clearfix">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className="ok">OK</button>
-          <button className="zero">0</button>
-          <button className="dot">.</button>
-        </div>
-      </NumberPadSection>
+      <NumberPad></NumberPad>
     </MyLayout>
   );
 }
